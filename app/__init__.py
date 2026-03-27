35971dd6df8cebf3bc0136dbbd0e91cf48d97583
bcx <2281270586@qq.com>
2026-03-27 18:42:53 +0800

    fix: add CSRFProtect to fix csrf_token undefined error in templates

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+csrf = CSRFProtect(app)
 
 UPLOAD_FOLDER = os.path.join(app.root_path, "uploads")
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
